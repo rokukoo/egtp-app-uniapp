b51da5c4e3b2aa15591882c91254ca218f3b2cb3
rokuko <rokuko88@gmail.com>
2023-11-25 11:26:23 +0800

    feat: 添加并配置pinia

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,41 +1,15 @@
 <template>
-  <view class="content">
-    <image mode="test"></image>
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
+  <view>
+    <uni-card title="你好">
+
+    </uni-card>
   </view>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Hello')
+
+
+
 </script>
 
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
-</style>
+<style scoped lang="scss"></style>
