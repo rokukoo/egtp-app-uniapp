45d4e433aac81c4762d28f3498d622d9822b94b1
rokuko <rokuko88@gmail.com>
2023-12-04 23:59:10 +0800

    feat: 初步完成热门物流子模块

---
+++ src/pages/logistics/hot-logistics-item.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <view class="bg-white rounded overflow-hidden shadow-md">
+    <view class="logo-image h-28">
+      <view
+        class="w-full h-full bg-gradient-to-br from-primary to-blue-300"
+      ></view>
+    </view>
+    <view classs="mx-2 py-2">
+      <view class="title"> XXXX跨国物流公司 </view>
+      <view class="phone">TEL: 138-xxxx-xxxx</view>
+    </view>
+  </view>
+</template>
---
+++ src/pages/logistics/hot-logistics.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import HotLogisticsItem from "./hot-logistics-item.vue";
+</script>
+
+<template>
+  <view class="flex gap-2">
+    <!-- 双栏式瀑布流 -->
+    <view class="col-1 flex-1 flex flex-col gap-4">
+      <hot-logistics-item
+        v-for="(item, index) in 10"
+        :key="`hot-logistics-item-c1-${index}`"
+      />
+    </view>
+    <view class="col-2 flex-1 flex flex-col gap-4">
+      <hot-logistics-item
+        v-for="(item, index) in 12"
+        :key="`hot-logistics-item-c2-${index}`"
+      />
+    </view>
+  </view>
+</template>
